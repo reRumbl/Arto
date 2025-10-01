54014d246e867e9e7e7adb848ce114a2d5db6509
Balashov Igor <igor2233balashov@gmail.com>
2025-10-01 09:55:33 +0300

    fix(backend): fix exceptions detail property

--- backend/src/core/exceptions.py
+++ backend/src/core/exceptions.py
@@ -3,7 +3,7 @@ from fastapi import HTTPException, status
 
 class BadRequestException(HTTPException):
     
-    def __init__(self, detail):
+    def __init__(self, detail: str | None = None):
         super().__init__(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=detail if detail else 'Bad request'
@@ -12,7 +12,7 @@ class BadRequestException(HTTPException):
 
 class UnauthorizedException(HTTPException):
     
-    def __init__(self, detail):
+    def __init__(self, detail: str | None = None):
         super().__init__(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail=detail if detail else 'Unauthorized'
@@ -21,7 +21,7 @@ class UnauthorizedException(HTTPException):
 
 class ForbiddenException(HTTPException):
     
-    def __init__(self, detail):
+    def __init__(self, detail: str | None = None):
         super().__init__(
             status_code=status.HTTP_403_FORBIDDEN,
             detail=detail if detail else 'Forbidden'
@@ -30,7 +30,7 @@ class ForbiddenException(HTTPException):
 
 class NotFoundException(HTTPException):
     
-    def __init__(self, detail):
+    def __init__(self, detail: str | None = None):
         super().__init__(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=detail if detail else 'Not found'
@@ -39,7 +39,7 @@ class NotFoundException(HTTPException):
 
 class InternalServerErrorException(HTTPException):
     
-    def __init__(self, detail):
+    def __init__(self, detail: str | None = None):
         super().__init__(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=detail if detail else 'Internal server error'
